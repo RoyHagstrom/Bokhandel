3004d14568b6636b9e67f6c691f19128ca6ab2e5
Roy.h@mail.com <Roy.h@mail.com>
2024-04-08 13:25:45 +0300

    Update footer.php

--- bokhandel/footer.php
+++ bokhandel/footer.php
@@ -1,2 +1,17 @@
+<footer class="bg-gray-900 text-white py-6">
+    <div class="container mx-auto flex justify-between items-center">
+        <a href="index.php" class="text-2xl font-bold">Bokhandel</a>
+        <nav>
+            <ul class="flex space-x-4">
+                <li><a href="index.php" class="hover:text-gray-400">Home</a></li>
+                <li><a href="#" class="hover:text-gray-400">Link 2</a></li>
+                <li><a href="#" class="hover:text-gray-400">Link 3</a></li>
+                <li><a href="#" class="hover:text-gray-400">Link 4</a></li>
+            </ul>
+        </nav>
+    </div>
+</footer>
+
+
 </body>
 </html>
