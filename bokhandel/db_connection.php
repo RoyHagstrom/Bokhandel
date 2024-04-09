cf73fd6feadfc3dd9389e832f1d2abdbc8492726
Roy.h@mail.com <Roy.h@mail.com>
2024-04-09 14:20:18 +0300

    14:20

--- bokhandel/db_connection.php
+++ bokhandel/db_connection.php
@@ -1,11 +1,24 @@
-
 <?php
-define('DB_HOST', 'novatest.ddns.net');
+define('DB_HOST_PRIMARY', 'novatest.ddns.net');
+define('DB_HOST_LOCAL', 'localhost');
 define('DB_PORT', '3306');
 define('DB_USERNAME', 'test');
 define('DB_PASSWORD', 'test');
 define('DB_DATABASE', 'bokhandel');
 
-$conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
-
+try {
+    $conn = new mysqli(DB_HOST_PRIMARY, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+    if ($conn->connect_error) {
+        throw new Exception("Connection to primary server failed: " . $conn->connect_error);
+    }
+} catch (Exception $e) {
+    try {
+        $conn = new mysqli(DB_HOST_LOCAL, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+        if ($conn->connect_error) {
+            throw new Exception("Connection to localhost failed: " . $conn->connect_error);
+        }
+    } catch (Exception $e) {
+        die("Failed to connect to database: " . $e->getMessage());
+    }
+}
 ?>
