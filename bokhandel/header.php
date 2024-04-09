4137587e35672c92717a71094159e500edd32ffa
Roy.h@mail.com <Roy.h@mail.com>
2024-04-09 16:56:55 +0300

    16:56

--- bokhandel/header.php
+++ bokhandel/header.php
@@ -26,6 +26,8 @@
                     <li><a href="books.php" class="text-white dark:text-gray-200 hover:text-gray-300">Books</a></li>
                     <li><a href="#" class="text-white dark:text-gray-200 hover:text-gray-300">Services</a></li>
                     <li><a href="contact.php" class="text-white dark:text-gray-200 hover:text-gray-300">Contact</a></li>
+                    <li><a href="login.php" class="bg-blue-900 text-white dark:text-gray-200 hover:bg-blue-800 dark:hover:bg-blue-400 px-4 py-2 rounded-md">Login</a></li>
+
                 </ul>
             </nav>
 
@@ -45,6 +47,8 @@
             <li><a href="books.php" class="text-white dark:text-gray-200 hover:text-gray-300">Books</a></li>
             <li><a href="#" class="text-white dark:text-gray-200 hover:text-gray-300">Services</a></li>
             <li><a href="contact.php" class="text-white dark:text-gray-200 hover:text-gray-300">Contact</a></li>
+            <li><a href="login.php" class="bg-blue-900 text-white dark:text-gray-200 hover:bg-blue-800 dark:hover:bg-blue-400 px-4 py-2 rounded-md">Login</a></li>
+
         </ul>
     </div>
 
