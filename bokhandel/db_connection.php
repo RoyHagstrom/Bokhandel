a7b4a03a7063b18ce29e446d30cd9a8089894e8f
Roy.h@mail.com <Roy.h@mail.com>
2024-04-09 10:25:18 +0300

    10:25

--- bokhandel/db_connection.php
+++ bokhandel/db_connection.php
@@ -8,12 +8,4 @@ define('DB_DATABASE', 'bokhandel');
 
 $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
 
-if ($conn->connect_error) {
-    $errorLogMessage = "[" . date('Y-m-d H:i:s') . "] Connection failed: " . $conn->connect_error . "\n";
-    file_put_contents('C:\xampp\htdocs\bokhandel\connection_error_log.txt', $errorLogMessage, FILE_APPEND);
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    $successLogMessage = "[" . date('Y-m-d H:i:s') . "] Connection successful\n";
-    file_put_contents('C:\xampp\htdocs\bokhandel\connection_success_log.txt', $successLogMessage, FILE_APPEND);
-}
 ?>
