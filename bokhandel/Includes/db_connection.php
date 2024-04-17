fb0b84e1cf56f00138d3e0506e34a77767933184
Roy.h@mail.com <Roy.h@mail.com>
2024-04-17 17:39:47 +0300

    17:39

--- bokhandel/Includes/db_connection.php
+++ bokhandel/Includes/db_connection.php
@@ -1,13 +1,15 @@
 <?php
 
 if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
-    die('We don\'t have mysqli!!!');
+    echo 'We don\'t have mysqli!!!';
+} else {
+    echo 'Phew we have it!';
 }
 
 define('DB_HOSTS', [
     'primary' => 'novatest.ddns.net',
     'local' => '192.168.1.111',
-    'localhost' => '192.168.128.2'
+    'localhost' => '172.22.0.2'
 ]);
 define('DB_PORT', '3306');
 define('DB_USERNAME', 'test');
@@ -15,36 +17,26 @@ define('DB_PASSWORD', 'test');
 define('DB_DATABASE', 'bokhandel');
 
 function connectToDb($host) {
-    try {
-        $conn = new mysqli($host, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
-        if ($conn->connect_error) {
-            throw new mysqli_sql_exception($conn->connect_error);
-        }
-        return $conn;
-    } catch (mysqli_sql_exception $e) {
-        throw $e;
+    $conn = new mysqli($host, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+    if ($conn->connect_error) {
+        throw new Exception("Connection failed: " . $conn->connect_error);
     }
+    return $conn;
 }
 
 function getDatabaseConnection() {
     try {
         return connectToDb(DB_HOSTS['primary']);
-    } catch (mysqli_sql_exception $e) {
+    } catch (Exception $e) {
         try {
             return connectToDb(DB_HOSTS['local']);
-        } catch (mysqli_sql_exception $e) {
+        } catch (Exception $e) {
             return connectToDb(DB_HOSTS['localhost']);
         }
     }
 }
 
 session_start();
-
-try {
-    $conn = getDatabaseConnection();
-    $user = new USER($conn);
-} catch (mysqli_sql_exception $e) {
-    echo "Error connecting to database: " . $e->getMessage();
-    exit;
-}
+$conn = getDatabaseConnection();
+$user = new USER($conn);
 
