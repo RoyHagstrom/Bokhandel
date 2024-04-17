5ab426fa734680eceafc654291e71e1c9ed23ed7
Roy.h@mail.com <Roy.h@mail.com>
2024-04-17 12:59:49 +0300

    12:59

--- bokhandel/Includes/db_connection.php
+++ bokhandel/Includes/db_connection.php
@@ -1,8 +1,7 @@
 <?php
 define('DB_HOSTS', [
     'primary' => 'novatest.ddns.net',
-    'local' => '192.168.1.111',
-    'localhost' => 'localhost'
+    'local' => '192.168.1.111'
 ]);
 define('DB_PORT', '3306');
 define('DB_USERNAME', 'test');
@@ -10,9 +9,12 @@ define('DB_PASSWORD', 'test');
 define('DB_DATABASE', 'bokhandel');
 
 function connectToDb($host) {
-    $conn = new mysqli($host, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
-    if ($conn->connect_error) {
-        throw new Exception("Connection failed: " . $conn->connect_error);
+    static $conn = null;
+    if ($conn === null) {
+        $conn = new mysqli($host, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+        if ($conn->connect_error) {
+            throw new Exception("Connection failed: " . $conn->connect_error);
+        }
     }
     return $conn;
 }
@@ -21,11 +23,7 @@ function getDatabaseConnection() {
     try {
         return connectToDb(DB_HOSTS['primary']);
     } catch (Exception $e) {
-        try {
-            return connectToDb(DB_HOSTS['local']);
-        } catch (Exception $e) {
-            return connectToDb(DB_HOSTS['localhost']);
-        }
+        return connectToDb(DB_HOSTS['local']);
     }
 }
 
