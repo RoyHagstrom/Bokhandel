63caa617f6bf366ce659e8f4f43ac944ca19455f
RoyHagstrom <114213765+RoyHagstrom@users.noreply.github.com>
2024-04-17 22:34:43 +0300

    22:34

--- bokhandel/edit_user.php
+++ bokhandel/edit_user.php
@@ -1,28 +1,34 @@
 <?php
 include 'Includes/header.php';
 
-
 if (!isset($_SESSION["uid"])) {
     $user->redirect("login.php");
 }
-
 
 $stmt = $conn->prepare("SELECT * FROM User WHERE UserID = ?");
 $stmt->bind_param("i", $_GET["userid"]);
 $stmt->execute();
 $userInfo = $stmt->get_result()->fetch_assoc();
 
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
     $username = isset($_POST["username"]) ? htmlspecialchars($_POST["username"]) : $userInfo["Username"];
     $email = isset($_POST["email"]) ? htmlspecialchars($_POST["email"]) : $userInfo["Email"];
 
-    
+    $stmt = $conn->prepare("SELECT COUNT(*) FROM User WHERE Username = ? AND UserID != ?");
+    $stmt->bind_param("si", $username, $_SESSION["uid"]);
+    $stmt->execute();
+    $result = $stmt->get_result()->fetch_row();
+
+
+    if ($result[0] > 0) {
+        echo "Username already exists";
+        exit();
+    }
+
     $stmt = $conn->prepare("UPDATE User SET Username = ?, Email = ? WHERE UserID = ?");
     $stmt->bind_param("ssi", $username, $email, $_SESSION["uid"]);
-    if ($stmt->execute()) {
 
+    if ($stmt->execute()) {
         $user->redirect("profile.php");
     } else {
         echo "Error updating user information: " . $stmt->error;
@@ -31,7 +37,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 
 <div class="dark min-h-screen bg-white text-gray-900 flex flex-col justify-center items-center">
-
     <div class="container mx-auto p-8">
         <h2 class="text-3xl font-bold mb-4 text-center">Edit Profile</h2>
         <form class="bg-white shadow-md rounded-lg p-8 mt-8" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
@@ -58,7 +63,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
         </form>
     </div>
-
 </div>
 
 <?php
