f67728619d934a1ece67568831f1f1846f33721f
Roy.h@mail.com <Roy.h@mail.com>
2024-04-17 17:53:12 +0300

    17:53

--- bokhandel/Includes/db_connection.php
+++ bokhandel/Includes/db_connection.php
@@ -1,41 +1,37 @@
 <?php
-
-if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
-    die('We don\'t have mysqli!!!' . PHP_EOL);
-}
-
-define('DB_HOSTS', [
-    'primary' => 'novatest.ddns.net',
-    'local' => '192.168.1.111',
-    'localhost' => '172.22.0.2'
-]);
-define('DB_PORT', '3306');
-define('DB_USERNAME', 'test');
-define('DB_PASSWORD', 'test');
-define('DB_DATABASE', 'bokhandel');
-
-function connectToDb($host) {
-    $conn = new mysqli($host, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
-    if ($conn->connect_error) {
-        throw new Exception("Connection failed: " . $conn->connect_error);
+    define('DB_HOSTS', [
+        'primary' => 'novatest.ddns.net',
+        'local' => '192.168.1.111',
+        'network' => '172.22.0.2'
+    ]);
+    define('DB_PORT', '3306');
+    define('DB_USERNAME', 'test');
+    define('DB_PASSWORD', 'test');
+    define('DB_DATABASE', 'bokhandel');
+    
+    function connectToDb($host) {
+        static $conn = null;
+        if ($conn === null) {
+            $conn = new mysqli($host, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+            if ($conn->connect_error) {
+                throw new Exception("Connection failed: " . $conn->connect_error);
+            }
+        }
+        return $conn;
     }
-    return $conn;
-}
-
-function getDatabaseConnection() {
-    try {
-        return connectToDb(DB_HOSTS['primary']);
-    } catch (Exception $e) {
+    
+    function getDatabaseConnection() {
         try {
-            return connectToDb(DB_HOSTS['local']);
+            return connectToDb(DB_HOSTS['primary']);
         } catch (Exception $e) {
-            return connectToDb(DB_HOSTS['localhost']);
+            try {
+                return connectToDb(DB_HOSTS['local']);
+            } catch (Exception $e) {
+                return connectToDb(DB_HOSTS['network']);
+            }
         }
     }
-}
-
-ob_start();
-session_start();
-$conn = getDatabaseConnection();
-$user = new USER($conn);
-
+    
+    session_start();
+    $conn = getDatabaseConnection();
+    $user = new USER($conn);
