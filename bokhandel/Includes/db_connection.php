5147f1c872cc55d013c78e1fbb70c963a3c2eb5f
Roy.h@mail.com <Roy.h@mail.com>
2024-04-17 16:19:46 +0300

    16:19

--- bokhandel/Includes/db_connection.php
+++ bokhandel/Includes/db_connection.php
@@ -1,9 +1,7 @@
 <?php
 
 if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
-    echo 'We don\'t have mysqli!!!';
-} else {
-    echo 'Phew we have it!';
+    die('We don\'t have mysqli!!!');
 }
 session_start();
 
