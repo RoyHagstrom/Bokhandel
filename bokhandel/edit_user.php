0fd3483ffcf9f6d0324ea5d4f5df8764dce89ff6
RoyHagstrom <114213765+RoyHagstrom@users.noreply.github.com>
2024-04-17 22:56:05 +0300

    22:56

--- bokhandel/edit_user.php
+++ bokhandel/edit_user.php
@@ -50,20 +50,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 
 <div class="dark min-h-screen bg-white text-gray-900 flex flex-col justify-center items-center">
+
     <div class="container mx-auto p-8">
-        <h2 class="text-3xl font-bold mb-4 text-center">Edit Profile</h2>
+        <h2 class="text-center text-3xl font-bold mb-4">Edit Profile</h2>
         <form class="bg-white shadow-md rounded-lg p-8 mt-8" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>?userid=<?php echo $userid; ?>">
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-4">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-8">
                 <div>
-                    <label class="block text-sm font-semibold mb-1" for="username">Username:</label>
+                    <label class="block mb-2 text-sm font-semibold" for="username">Username:</label>
                     <input type="text" id="username" name="username" class="border border-gray-400 p-2 w-full rounded-md" value="<?php echo htmlspecialchars($userInfo['Username']); ?>" required>
                 </div>
                 <div>
-                    <label class="block text-sm font-semibold mb-1" for="email">Email:</label>
+                    <label class="block mb-2 text-sm font-semibold" for="email">Email:</label>
                     <input type="email" id="email" name="email" class="border border-gray-400 p-2 w-full rounded-md" value="<?php echo htmlspecialchars($userInfo['Email']); ?>" required>
                 </div>
                 <div>
-                    <label class="block text-sm font-semibold mb-1" for="role">Role:</label>
+                    <label class="block mb-2 text-sm font-semibold" for="role">Role:</label>
                     <select id="role" name="role" class="border border-gray-400 p-2 w-full rounded-md">
                         <option value="Admin" <?php echo $userInfo['Role'] == 'Admin' ? 'selected' : ''; ?>>Admin</option>
                         <option value="Regular" <?php echo $userInfo['Role'] == 'Regular' ? 'selected' : ''; ?>>Regular</option>
@@ -71,11 +72,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 </div>
             </div>
 
-            <div class="text-center mt-6">
-                <button type="submit" class="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-md">Update</button>
+            <div class="text-center mt-8">
+                <button type="submit" class="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-md shadow-md">Update</button>
             </div>
         </form>
     </div>
+
 </div>
 
 <?php
