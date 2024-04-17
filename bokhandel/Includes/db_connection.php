c8142b378cd37a49ac1b7586f418d3c448ff57c6
Roy.h@mail.com <Roy.h@mail.com>
2024-04-17 13:01:44 +0300

    13:01

--- bokhandel/Includes/db_connection.php
+++ bokhandel/Includes/db_connection.php
@@ -1,7 +1,8 @@
 <?php
 define('DB_HOSTS', [
     'primary' => 'novatest.ddns.net',
-    'local' => '192.168.1.111'
+    'local' => '192.168.1.111',
+    'localhost' => '192.168.1.111'
 ]);
 define('DB_PORT', '3306');
 define('DB_USERNAME', 'test');
@@ -9,12 +10,9 @@ define('DB_PASSWORD', 'test');
 define('DB_DATABASE', 'bokhandel');
 
 function connectToDb($host) {
-    static $conn = null;
-    if ($conn === null) {
-        $conn = new mysqli($host, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
-        if ($conn->connect_error) {
-            throw new Exception("Connection failed: " . $conn->connect_error);
-        }
+    $conn = new mysqli($host, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+    if ($conn->connect_error) {
+        throw new Exception("Connection failed: " . $conn->connect_error);
     }
     return $conn;
 }
@@ -23,11 +21,16 @@ function getDatabaseConnection() {
     try {
         return connectToDb(DB_HOSTS['primary']);
     } catch (Exception $e) {
-        return connectToDb(DB_HOSTS['local']);
+        try {
+            return connectToDb(DB_HOSTS['local']);
+        } catch (Exception $e) {
+            return connectToDb(DB_HOSTS['localhost']);
+        }
     }
 }
 
 session_start();
 $conn = getDatabaseConnection();
+require_once 'classes/User.php';
 $user = new USER($conn);
 
