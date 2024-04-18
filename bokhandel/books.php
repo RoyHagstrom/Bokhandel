baa6bdf51b92d492ca4f878b0dab2fcbe4503800
Roy.h@mail.com <Roy.h@mail.com>
2024-04-18 14:04:01 +0300

    14:03

--- bokhandel/books.php
+++ bokhandel/books.php
@@ -177,7 +177,7 @@ if ($category_id) {
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                         </svg>
                     </div>
-                    <input type="search" <?php if(isset($_GET['search'])){echo 'value="' . htmlspecialchars($_GET['search']) . '"';} ?>  id="default-search" name="search" class="block w-full p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+                    <input type="search" <?php if(isset($_GET['search'])) { echo 'value="' . htmlspecialchars(urldecode($_GET['search'])) . '"'; } ?>  id="default-search" name="search" class="block w-full p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
                 </div>
                 <button type="submit" class="text-white ml-2 bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Search
