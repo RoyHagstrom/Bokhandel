7bc40811ea6619ff892683d7efd6c5d6ae74487f
RoyHagstrom <114213765+RoyHagstrom@users.noreply.github.com>
2024-04-21 13:16:52 +0300

    13:16

--- bokhandel/Includes/db_connection.php
+++ bokhandel/Includes/db_connection.php
@@ -1,46 +1,67 @@
 <?php
 
-    if (!defined('DB_HOSTS')) {
-        define('DB_HOSTS', [
-            'primary' => 'novatest.ddns.net',
-            'local' => '192.168.1.111',
-            'network' => '172.22.0.2'
-        ]);
-        define('DB_PORT', '3306');
-        define('DB_USERNAME', 'test');
-        define('DB_PASSWORD', 'test');
-        define('DB_DATABASE', 'bokhandel');
+define('DB_HOSTS', [
+    'primary' => '172.22.0.2',
+    'local' => '192.168.1.111',
+    'network' => 'novatest.ddns.net'
+]);
+
+define('DB_PORT', '3306');
+define('DB_USERNAME', 'test');
+define('DB_PASSWORD', 'test');
+define('DB_DATABASE', 'bokhandel');
+
+function connectToDb($host) {
+    static $conns = [];
+    if (isset($conns[$host])) {
+        return $conns[$host];
     }
 
-    function connectToDb($host) {
-        static $conn_cache = []; 
-        if (!isset($conn_cache[$host])) {
-            $conn_cache[$host] = new mysqli($host, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
-            if ($conn_cache[$host]->connect_error) {
-                throw new Exception("Connection failed: " . $conn_cache[$host]->connect_error);
-            }
-        }
-        return $conn_cache[$host];
+    $mysqli = new mysqli($host, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+
+    if ($mysqli->connect_error) {
+
+        error_log('Connection error: ' . $mysqli->connect_error);
+        throw new Exception('Could not connect to the database.');
     }
 
+    $mysqli->set_charset('utf8mb4');
+    $mysqli->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, true);
+    $mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 1);
 
-    function getDatabaseConnection() {
+    $conns[$host] = $mysqli;
+    return $mysqli;
+}
+
+function getDatabaseConnection() {
+    $hosts = DB_HOSTS;
+    foreach ($hosts as $host) {
         try {
-            return connectToDb(DB_HOSTS['primary']);
+            return connectToDb($host);
         } catch (Exception $e) {
-            try {
-                return connectToDb(DB_HOSTS['local']);
-            } catch (Exception $e) {
-                return connectToDb(DB_HOSTS['network']);
-            }
+            continue;
         }
     }
+    throw new Exception('Could not connect to any database host.');
+}
 
-    if (!session_status() !== PHP_SESSION_ACTIVE) {
-        session_start();
-    }
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start([
+        'cookie_lifetime' => 3600,
+        'cookie_httponly' => true,
+        'cookie_samesite' => 'Strict',
+        'cookie_secure' => true,
+    ]);
+}
 
+try {
     $conn = getDatabaseConnection();
+
     $user = new USER($conn);
+} catch (Exception $e) {
 
+    error_log('Database connection error: ' . $e->getMessage());
 
+    exit('Oops! Something went wrong. Please try again later.');
+}
+?>
