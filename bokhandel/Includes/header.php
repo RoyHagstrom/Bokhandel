b0c24cffb8b1731583f7281896bea7c205278e84
RoyHagstrom <114213765+RoyHagstrom@users.noreply.github.com>
2024-04-21 13:28:45 +0300

    13:28

--- bokhandel/Includes/header.php
+++ bokhandel/Includes/header.php
@@ -12,6 +12,7 @@ include 'db_connection.php';
 <head>
 
 
+
 <meta http-equiv="Cache-Control" content="max-age=3600, must-revalidate">
 
 
@@ -53,8 +54,8 @@ include 'db_connection.php';
     <link rel="preconnect" href="https://www.google-analytics.com">
     <link rel="preconnect" href="https://www.googleapis.com">
     <link rel="preconnect" href="https://www.google.com">
-    <link rel="preload" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19">
-    <link rel="preload" href="https://cdn.jsdelivr.net/npm/daisyui@1.10.0">
+    <link rel="preload" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" as="style" crossorigin="anonymous" integrity="sha256-X+Xsy71qW+1KW4bC/BzGW0S6Fk/4NvA+l4X6cA0v6U=">
+    <link rel="preload" href="https://cdn.jsdelivr.net/npm/daisyui@1.10.0/dist/full.css" as="style" crossorigin="anonymous" integrity="sha256-4c7yQWXx7t3C0t/Nj/o32/FZQMjG2YrOcGQQNx/2jY=">
 
 
     <meta charset="UTF-8">
