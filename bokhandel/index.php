4bab8d7826808dc8cc55ba1a0fa124875a6c809a
RoyHagstrom <114213765+RoyHagstrom@users.noreply.github.com>
2024-04-22 19:23:57 +0300

    19:23

--- bokhandel/index.php
+++ bokhandel/index.php
@@ -134,9 +134,8 @@ $featured_books_result = $conn->query($featured_books_sql);
     }
 </script>
 
-<div class="rounded-lg shadow-md">
 <h1 class="text-2xl font-semibold mb-6 text-center text-black">Book Series</h1>
-<div class="container mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4 md:gap-8 place-content-center items-center">
+<div class="container mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4 md:gap-8 place-content-center items-center rounded-lg shadow-md">
     <?php
     $stmt = $conn->query("SELECT * FROM Series LIMIT 4");
     while ($series = $stmt->fetch_assoc()) { ?>
@@ -153,7 +152,6 @@ $featured_books_result = $conn->query($featured_books_sql);
             <p class="text-gray-700 dark:text-gray-300">No book series available</p>
         </div>
     <?php } ?>
-</div>
 </div>
 
 
