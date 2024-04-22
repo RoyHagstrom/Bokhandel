0f040c26fe3b30e8b6f1d35d522d121a0de64e51
Roy.h@mail.com <Roy.h@mail.com>
2024-04-22 16:04:25 +0300

    16:04

--- bokhandel/manage_series.php
+++ bokhandel/manage_series.php
@@ -60,7 +60,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         } else {
             $image = $_FILES['series_image']['tmp_name'] ?? '';
             $image_name = $_FILES['series_image']['name'] ?? '';
-            $target_dir = "Uploads/series/";
+            $target_dir = "/images";
             $target_file = $target_dir . basename($image_name);
             $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
