383c8c569d3d934c66f8e19c58ed7f135767f7ad
Roy.h@mail.com <Roy.h@mail.com>
2024-04-22 12:48:23 +0300

    12:48

--- bokhandel/Includes/db_connection.php
+++ bokhandel/Includes/db_connection.php
@@ -9,17 +9,24 @@
     define('DB_USERNAME', 'test');
     define('DB_PASSWORD', 'test');
     define('DB_DATABASE', 'bokhandel');
+    define('DB_CONNECTION_TIMEOUT', 3);
 
 
     function getDatabaseConnection() {
+        $startTime = time();
         foreach (DB_HOSTS as $host) {
-            $conn = new mysqli($host, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+            $conn = new mysqli($host, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT, null, MYSQLI_CLIENT_FOUND_ROWS);
             if (!$conn->connect_error) {
+                $conn->options(MYSQLI_OPT_CONNECT_TIMEOUT, DB_CONNECTION_TIMEOUT);
+                if ($conn->connect_error && (time() - $startTime < DB_CONNECTION_TIMEOUT)) {
+                    continue;
+                }
                 return $conn;
             }
         }
         throw new Exception("All database hosts failed to connect");
     }
+
 
     if (session_status() !== PHP_SESSION_ACTIVE) {
         session_start();
