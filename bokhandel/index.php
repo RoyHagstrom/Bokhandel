518c0b262706302e0456e6ea39e79a72b7ed1759
Roy.h@mail.com <Roy.h@mail.com>
2024-04-22 17:33:03 +0300

    17:33

--- bokhandel/index.php
+++ bokhandel/index.php
@@ -162,43 +162,9 @@ $featured_books_result = $conn->query($featured_books_sql);
     </div>
 </div>
 
-<!--
-<div class="container mt-8">
-    <h1 class="text-2xl font-semibold mb-6">Book Series</h1>
-    <?php
-    $series_query = "SELECT DISTINCT Series FROM Book";
-    $series_result = $conn->query($series_query);
 
-    if ($series_result->num_rows > 0) {
-        while ($series = $series_result->fetch_assoc()) {
-            echo "<h2 class='text-xl font-semibold mb-2'>${series['BookSeries']}</h2>";
-            $series_name = $series['BookSeries'];
 
-            $book_series_query = "SELECT * FROM Book WHERE Series = '$series_name'";
-            $book_series_result = $conn->query($book_series_query);
 
-            if ($book_series_result->num_rows > 0) {
-                while ($book = $book_series_result->fetch_assoc()) {
-                    echo '<a href="singlebook.php?id=' . $book['BookID'] . '" class="block bg-gray-100 dark:bg-gray-700 rounded-lg shadow-md overflow-hidden hover:bg-gray-200 dark:hover:bg-gray-800 transition duration-300 relative">';
-                    echo '<span class="absolute top-2 right-2 bg-white text-gray-900 font-semibold px-2 py-1 rounded-lg">' . $book['Price'] . '€</span>';
-                    echo '<img src="' . $book['Image'] . '" alt="' . $book['Title'] . '" class="w-30 md:w-full h-30 md:h-80 object-cover">';
-                    echo '<div class="p-3 md:p-6 text-sm md:text-md">';
-                    echo '<h2 class="md:text-xl font-semibold text-gray-900 dark:text-white">' . $book['Title'] . '</h2>';
-                    echo '<p class="text-gray-700 dark:text-gray-300">Author: ' . $book['Author'] . '</p>';
-                    echo '<p class="text-gray-700 dark:text-gray-300">' . htmlspecialchars(substr(strip_tags(html_entity_decode($book["Description"])), 0, 50)) . '...</p>';
-                    echo '</div>';
-                    echo '</a>';
-                }
-            } else {
-                echo 'No books in this series';
-            }
-        }
-    } else {
-        echo 'No book series available';
-    }
-    ?>
-</div>
--->
 
 <div class="bg-white container p-8 rounded-lg shadow-md w-full sm:w-130 mt-8">
     <h1 class="text-2xl font-semibold mb-6">New Books</h1>
