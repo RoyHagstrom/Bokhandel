d30d0914703b834b2dd5237a23d2b09d3c0edf65
RoyHagstrom <114213765+RoyHagstrom@users.noreply.github.com>
2024-04-22 19:27:23 +0300

    19:27

--- bokhandel/index.php
+++ bokhandel/index.php
@@ -144,7 +144,7 @@ $featured_books_result = $conn->query($featured_books_sql);
         <div class="relative overflow-hidden rounded-lg shadow-md">
             <img src="<?= $series['Image'] ?>" alt="<?= $series['SeriesName'] ?>" class="w-full h-48 sm:h-64 lg:h-80 object-cover group-hover:opacity-75 transition-opacity duration-200 ease-in-out" />
             <div class="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-40 group-hover:bg-opacity-75 transition-opacity duration-200 ease-in-out">
-                <h2 class="text-2xl font-semibold text-white mb-2 group-hover:opacity-100 transition-opacity duration-200 ease-in-out underline decoration-sky-500"><?= $series['SeriesName'] ?></h2>
+                <h2 class="text-2xl font-semibold text-white mb-2 group-hover:opacity-100 transition-opacity duration-200 ease-in-out underline decoration-black"><?= $series['SeriesName'] ?></h2>
             </div>
         </div>
     </a>
