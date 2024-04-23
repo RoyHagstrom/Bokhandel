97a67e6b7ed7a77c2de4fe26d9eb326be6988da3
Roy.h@mail.com <Roy.h@mail.com>
2024-04-23 13:03:28 +0300

    13:03

--- bokhandel/singlebook.php
+++ bokhandel/singlebook.php
@@ -156,16 +156,11 @@ $new_books_result = $conn->query($new_books_sql);
     <?php
     $user_bio_sql = "SELECT Bio, Username FROM User WHERE Username = '{$book['Author']}'";
     $user_bio_result = $conn->query($user_bio_sql);
-    if ($user_bio_result->num_rows > 0) {
-        $user_bio_row = $user_bio_result->fetch_assoc();
-        $user_bio = $user_bio_row; 
-    } else {
-        $user_bio = "";
-    }
+    $user_bio = $user_bio_result->fetch_assoc();
     ?>
     <?php if (!empty($user_bio["Bio"])): ?>
     <div class="p-8 rounded-lg w-full sm:w-116 mt-8 bg-gray-100">
-        <h1 class=" font-semibold mb-6 text-center text-black sm:text-3xl text-2xl"><?php $user_bio['Username'] ?></h1>
+        <h1 class=" font-semibold mb-6 text-center text-black sm:text-3xl text-2xl"><?php echo $user_bio['Username'] ?></h1>
         <p><?php echo $user_bio["Bio"]; ?></p>
     </div>
     <?php endif; ?>
