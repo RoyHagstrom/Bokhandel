26d3a236800bdeea0aa6b2a03fa2003a1f3dc1a5
Roy.h@mail.com <Roy.h@mail.com>
2024-04-23 14:25:30 +0300

    14:25

--- bokhandel/Includes/header.php
+++ bokhandel/Includes/header.php
@@ -288,7 +288,6 @@ include 'db_connection.php';
 
 
             <body class="full bg-gray-800 dark:bg-gray-900">
-                <div class="max-w-7xl overflow-auto bg-gradient-to-t from-gray-800 to-gray-900">
-
+                <div class="max-w-7xl mx-auto flex justify-center overflow-auto bg-gray-800 dark:bg-gray-900">
 
 
