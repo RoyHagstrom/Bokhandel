72ed353ba4d446f1f2589b3cb4aa5a6f45b9a708
Roy.h@mail.com <Roy.h@mail.com>
2024-04-23 15:50:59 +0300

    15:50

--- bokhandel/Includes/class.user.php
+++ bokhandel/Includes/class.user.php
@@ -44,8 +44,21 @@ class USER
             }
 
             $searchTerm = '%' . $this->conn->real_escape_string(trim($searchTerm)) . '%';
-            $stmt = $this->conn->prepare("SELECT * FROM Book WHERE Title LIKE ? OR Author LIKE ? ORDER BY BookID DESC LIMIT 4");
-            $stmt->bind_param("ss", $searchTerm, $searchTerm);
+            $likeClauses = array();
+            for($i = 0; $i < 2; $i++) {
+                $likeClauses[] = "(?:Title|Author) LIKE ?";
+            }
+            $stmt = $this->conn->prepare("SELECT * FROM Book WHERE " . implode(' OR ', $likeClauses) . " ORDER BY BookID DESC LIMIT 4");
+            $searchTerms = array_fill(0, 4, $searchTerm);
+            array_walk($searchTerms, function(&$value, $index) use ($searchTerm) {
+                if(preg_match('/\s/', $value)) {
+                    $words = explode(' ', $value);
+                    $lastWord = array_pop($words);
+                    $value = implode(' ', $words) . '%' . str_repeat('_', strlen($lastWord)) . str_repeat('[a-z]', strlen($lastWord));
+                }
+            });
+            $searchTerms = array_merge($searchTerms, array_fill(0, 2, $searchTerm));
+            call_user_func_array([$stmt, 'bind_param'], array_merge(array(str_repeat('s', strlen($searchTerms))), $searchTerms));
             $stmt->execute() or die($this->conn->error);
             $result = $stmt->get_result();
             $books = $result->fetch_all(MYSQLI_ASSOC);
