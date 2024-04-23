1e40226dbdccd2acb56a3406330fa0b78f67a6b6
Roy.h@mail.com <Roy.h@mail.com>
2024-04-23 14:28:04 +0300

    14:28

--- bokhandel/Includes/header.php
+++ bokhandel/Includes/header.php
@@ -288,6 +288,5 @@ include 'db_connection.php';
 
 
             <body class="full bg-gray-800 dark:bg-gray-900">
-                <div class="max-w-screen-2xl mx-auto flex justify-center overflow-auto bg-gray-800 dark:bg-gray-900">
-
+                <div style="width: 1920px;" class="max-w-full mx-auto flex justify-center overflow-auto bg-gray-800 dark:bg-gray-900">
 
