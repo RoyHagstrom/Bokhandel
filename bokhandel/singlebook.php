2c0983f367faa158dee421d8608d28e3a84b79ae
Roy.h@mail.com <Roy.h@mail.com>
2024-04-23 13:06:56 +0300

    13:06

--- bokhandel/singlebook.php
+++ bokhandel/singlebook.php
@@ -96,7 +96,6 @@ $new_books_result = $conn->query($new_books_sql);
                 echo '<img src="' . $other_book['Image'] . '" alt="' . $other_book['Title'] . '" class="w-full md:w-full h-28 md:h-48 object-cover">';
                 echo '<div class="p-3 md:p-6 text-sm md:text-md">';
                 echo '<h2 class="md:text-xl font-semibold text-gray-900 dark:text-white">' . $other_book['Title'] . '</h2>';
-                echo '<p class="text-gray-700 dark:text-gray-300">Author: ' . $other_book['Author'] . '</p>';
                 echo '</div>';
                 echo '</a>';
 
