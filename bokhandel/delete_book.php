0ec49cf70c21f9187af0bb4364b0245257f3562f
Roy.h@mail.com <Roy.h@mail.com>
2024-04-24 16:20:11 +0300

    16:20

--- bokhandel/delete_book.php
+++ bokhandel/delete_book.php
@@ -32,7 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($stmt->execute()) {
         //unlink($file);
-        $user->redirect("my_books.php?uid=" . $_SESSION["uid"]);
+        $user->redirect("my_books.php?uid=" . $_SESSION["uname"]);
     } else {
         echo "Error deleting book: " . $stmt->error;
     }
