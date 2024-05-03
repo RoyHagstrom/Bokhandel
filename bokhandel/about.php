ff5673d0d5a7dd22afc8aab1a2ddd74bb3481f7e
Roy.h@mail.com <Roy.h@mail.com>
2024-05-03 10:11:35 +0300

    10:11

--- bokhandel/about.php
+++ bokhandel/about.php
@@ -4,9 +4,11 @@ include 'Includes/header.php';
 
 <div class="bg-white text-black px-4 py-20 flex flex-col justify-center items-center sm:px-6 lg:px-8">
 
+
+<div class="mx-auto w-full rounded-lg shadow-md max-w-[800px] flex flex-col gap-4 sm:flex-row sm:gap-0 sm:px-8 rounded-lg p-8 m-8">
+
     <h1 class="text-2xl font-bold mb-4 sm:text-3xl">About Us</h1>
 
-    <div class="mx-auto w-full rounded-lg shadow-md max-w-[800px] flex flex-col gap-4 sm:flex-row sm:gap-0 sm:px-8 rounded-lg">
         <p class="leading-relaxed">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus, dolor eget suscipit facilisis, magna felis tempus ligula, at eleifend mi nibh eu nunc. Nullam auctor, velit in fermentum bibendum, metus odio malesuada nulla, eu suscipit nisl nisi in tortor. Ut vel purus est. Sed tincidunt tincidunt mi, ac tincidunt nunc feugiat ac. Vivamus mattis nulla eu nisl rhoncus, vitae cursus justo lacinia. Sed feugiat, elit non vulputate aliquet, ante purus cursus nisl, vel lobortis risus nunc eu mi.
         </p>
