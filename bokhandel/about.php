4d7557863fae9913dff3a5f6ede506331fa0f3b7
Roy.h@mail.com <Roy.h@mail.com>
2024-05-03 10:50:11 +0300

    10:50

--- bokhandel/about.php
+++ bokhandel/about.php
@@ -42,54 +42,47 @@
         <div class="mt-8">
 
 
-
         <section id="content" role="main" class="content-wrapper">
     <div class="content-wrapper">
         <article class="post-type-page" id="post-288">
             <section class="article-content">
                 <div class="article-body page-body pt-4">
-                    <h1 class="aka-display aka-title title mb-4">Historia</h1>
-                    <p class="has-primary-1-color has-text-color">Akademiska Bokhandeln blev en del av Bonnier Books i oktober 2015 och har sedan dess fortsatt att fungera självständigt som en inspirerande mötesplats för alla bokälskare. Vi är engagerade i läsning och livslångt lärande. Även vår långa historia säger det – böcker har alltid haft en speciell plats i våra hjärtan. <br><br>Akademens webbplats har uppdaterats flera gånger under åren, senast i oktober 2021. Akademens logo och visuella utseende förnyades i samband med företagets brandförnyelse. Vår förhoppning med förnyelsen är att Akademen ska kunna betjäna sina kunder och samarbetspartners ännu bättre.</p>
-                    <h2 class="wp-block-heading">1890-1930-talet </h2>
+                    <h1 class="text-3xl font-bold mb-4">Historia</h1>
+                    <p class="text-primary mb-8">Akademiska Bokhandeln blev en del av Bonnier Books i oktober 2015 och har sedan dess fortsatt att fungera självständigt som en inspirerande mötesplats för alla bokälskare. Vi är engagerade i läsning och livslångt lärande. Även vår långa historia säger det – böcker har alltid haft en speciell plats i våra hjärtan.</p>
+                    <h2 class="text-2xl font-bold mb-4">1890-1930-talet</h2>
                     <p>Akademiska Bokhandeln grundades sommaren 1893, och hade från första början som målsättning att kunna erbjuda sina kunder ett brett urval litteratur på både finska och svenska, och dessutom även utländska titlar, vilket var en sällsynthet i Helsingfors på den tiden. För Akademens grundare Alvar Renqvist och Gösta Branders var bokhandeln inte bara ett företag, utan även ett bidrag för folkbildningen.</p>
-                    <div class="aka-block">
-                        <div class="aka-double-column gx-0 aka-image-by-text type-1-3 align-left row depth-0 align-items-center">
-                            <div class="col-12 col-md-6 col-lg-9 column-left">
-                                <img srcset="https://www.akateeminen.com/wp-content/uploads/2021/10/Akateeminen_aleksanterinkadulla1920-150x150.jpeg 150w, https://www.akateeminen.com/wp-content/uploads/2021/10/Akateeminen_aleksanterinkadulla1920-350x257.jpeg 350w, https://www.akateeminen.com/wp-content/uploads/2021/10/Akateeminen_aleksanterinkadulla1920-500x368.jpeg 500w, https://www.akateeminen.com/wp-content/uploads/2021/10/Akateeminen_aleksanterinkadulla1920-768x565.jpeg 768w, https://www.akateeminen.com/wp-content/uploads/2021/10/Akateeminen_aleksanterinkadulla1920-1024x753.jpeg 1024w, https://www.akateeminen.com/wp-content/uploads/2021/10/Akateeminen_aleksanterinkadulla1920.jpeg 1444w" sizes="(min-width: 0px) and (max-width: 575px) calc(100vw / (12 / 12)), (min-width: 576px) and (max-width: 767px) 540px, (min-width: 768px) and (max-width: 991px) 360px, (min-width: 992px) and (max-width: 1199px) 480px, (min-width: 1200px) and (max-width: 1399px) 570px, (min-width: 1400px) 720px" alt="" style="object-position: 50% 50%;">
-                            </div>
-                            <div class="col-12 col-md-6 col-lg-3 column-right">
-                                <p>Gösta Branders hade rest runt i Europa och under sina affärsresor bekantat sig med bokaffärer nere på kontinenten, och tog med sig av dessa influenser till Akademen. Utrymmen skulle vara rymliga, urvalet brett och böckerna framme för kunderna att bläddra i. Den kosmopolitiska stämningen och det breda utbudet av europeisk litteratur gjorde Akademen populär bland universitetsfolket och stadens intellektuella.</p>
-                            </div>
+                    <div class="flex flex-wrap items-center mb-8">
+                        <div class="w-full md:w-1/2 lg:w-3/4 pr-0 md:pr-8">
+                            <img src="https://www.akateeminen.com/wp-content/uploads/2021/10/Akateeminen_aleksanterinkadulla1920.jpeg" alt="" class="w-full">
+                        </div>
+                        <div class="w-full md:w-1/2 lg:w-1/4 pl-0 md:pl-8">
+                            <p class="mb-0">Gösta Branders hade rest runt i Europa och under sina affärsresor bekantat sig med bokaffärer nere på kontinenten, och tog med sig av dessa influenser till Akademen. Utrymmen skulle vara rymliga, urvalet brett och böckerna framme för kunderna att bläddra i. Den kosmopolitiska stämningen och det breda utbudet av europeisk litteratur gjorde Akademen populär bland universitetsfolket och stadens intellektuella.</p>
                         </div>
                     </div>
-                    <div class="aka-block">
-                        <div class="aka-double-column gx-0 aka-image-by-text type-1-3 align-right row depth-0 align-items-center">
-                            <div class="col-12 col-md-6 col-lg-3 column-right">
-                                <p>Akademens första butik öppnades på Alexandersgatan 9, men Branders tyckte snart den blev för liten för hans vision. Under 1900-talets första årtionden flyttade bokhandeln runt på ett par olika adresser på Alexandersgatan, innan Akademen 1930 blev uppköpt av OY Stockmann AB och då flyttade in i Stockmanns varuhus på Alexandersgatan 52.</p>
-                            </div>
-                            <div class="col-12 col-md-6 col-lg-9 column-left">
-                                <img srcset="https://www.akateeminen.com/wp-content/uploads/2021/10/jouluaatto_aleksanterinkadulla1924-150x150.jpeg 150w, https://www.akateeminen.com/wp-content/uploads/2021/10/jouluaatto_aleksanterinkadulla1924-350x258.jpeg 350w, https://www.akateeminen.com/wp-content/uploads/2021/10/jouluaatto_aleksanterinkadulla1924-500x369.jpeg 500w, https://www.akateeminen.com/wp-content/uploads/2021/10/jouluaatto_aleksanterinkadulla1924-768x567.jpeg 768w, https://www.akateeminen.com/wp-content/uploads/2021/10/jouluaatto_aleksanterinkadulla1924-1024x756.jpeg 1024w, https://www.akateeminen.com/wp-content/uploads/2021/10/jouluaatto_aleksanterinkadulla1924-scaled.jpeg 2048w" sizes="(min-width: 0px) and (max-width: 575px) calc(100vw / (12 / 12)), (min-width: 576px) and (max-width: 767px) 540px, (min-width: 768px) and (max-width: 991px) 360px, (min-width: 992px) and (max-width: 1199px) 480px, (min-width: 1200px) and (max-width: 1399px) 570px, (min-width: 1400px) 720px" alt="" style="object-position: 50% 50%;">
-                            </div>
+                    <h2 class="text-2xl font-bold mb-4">1930-1971</h2>
+                    <div class="flex flex-wrap items-center mb-8">
+                        <div class="w-full md:w-1/2 lg:w-1/4 pr-0 md:pr-8 order-2 md:order-1">
+                            <p class="mb-0">Akademens första butik öppnades på Alexandersgatan 9, men Branders tyckte snart den blev för liten för hans vision. Under 1900-talets första årtionden flyttade bokhandeln runt på ett par olika adresser på Alexandersgatan, innan Akademen 1930 blev uppköpt av OY Stockmann AB och då flyttade in i Stockmanns varuhus på Alexandersgatan 52.</p>
+                        </div>
+                        <div class="w-full md:w-1/2 lg:w-3/4 pl-0 md:pl-8 order-1 md:order-2">
+                            <img src="https://www.akateeminen.com/wp-content/uploads/2021/10/jouluaatto_aleksanterinkadulla1924-scaled.jpeg" alt="" class="w-full">
                         </div>
                     </div>
-                    <div class="aka-block">
-                        <div class="aka-double-column gx-0 aka-image-by-text type-1-3 align-left row depth-0 align-items-center">
-                            <div class="col-12 col-md-6 col-lg-9 column-left">
-                                <img srcset="https://www.akateeminen.com/wp-content/uploads/2021/10/Aleksanterinkatu1966-150x150.jpeg 150w, https://www.akateeminen.com/wp-content/uploads/2021/10/Aleksanterinkatu1966-350x254.jpeg 350w, https://www.akateeminen.com/wp-content/uploads/2021/10/Aleksanterinkatu1966-500x363.jpeg 500w, https://www.akateeminen.com/wp-content/uploads/2021/10/Aleksanterinkatu1966-768x558.jpeg 768w, https://www.akateeminen.com/wp-content/uploads/2021/10/Aleksanterinkatu1966-1024x744.jpeg 1024w, https://www.akateeminen.com/wp-content/uploads/2021/10/Aleksanterinkatu1966.jpeg 1568w" sizes="(min-width: 0px) and (max-width: 575px) calc(100vw / (12 / 12)), (min-width: 576px) and (max-width: 767px) 540px, (min-width: 768px) and (max-width: 991px) 360px, (min-width: 992px) and (max-width: 1199px) 480px, (min-width: 1200px) and (max-width: 1399px) 570px, (min-width: 1400px) 720px" alt="" style="object-position: 50% 50%;">
-                            </div>
-                            <div class="col-12 col-md-6 col-lg-3 column-right">
-                                <p>Akademens flaggskeppsbutik låg på Alexandersgatan 52 i Stockmanns varuhus fram till 1971, då Akademen flyttade till en helt egen fastighet på adressen Universitetsgatan 17. Butiken ökade i storlek och sortiment, och blev ett landmärke för både bokälskare och turister.</p>
-                            </div>
+                    <h2 class="text-2xl font-bold mb-4">Från 1971</h2>
+                    <div class="flex flex-wrap items-center mb-8">
+                        <div class="w-full md:w-1/2 lg:w-3/4 pr-0 md:pr-8">
+                            <img src="https://www.akateeminen.com/wp-content/uploads/2021/10/Aleksanterinkatu1966.jpeg" alt="" class="w-full">
+                        </div>
+                        <div class="w-full md:w-1/2 lg:w-1/4 pl-0 md:pl-8">
+                            <p class="mb-0">Akademens flaggskeppsbutik låg på Alexandersgatan 52 i Stockmanns varuhus fram till 1971, då Akademen flyttade till en helt egen fastighet på adressen Universitetsgatan 17. Butiken ökade i storlek och sortiment, och blev ett landmärke för både bokälskare och turister.</p>
                         </div>
                     </div>
-                    <div class="aka-block">
-                        <div class="aka-double-column gx-0 aka-image-by-text type-1-3 align-right row depth-0 align-items-center">
-                            <div class="col-12 col-md-6 col-lg-3 column-right">
-                                <p>Första butikens ledord ”Litteraturen är livets krydda” blev även hela Akademens affärsidé – att erbjuda kvalitetsböcker, högklassig service och inspiration för sin kundkrets.</p>
-                            </div>
-                            <div class="col-12 col-md-6 col-lg-9 column-left">
-                                <img srcset="https://www.akateeminen.com/wp-content/uploads/2021/10/Aleksanterinkatu_1979-150x150.jpeg 150w, https://www.akateeminen.com/wp-content/uploads/2021/10/Aleksanterinkatu_1979-350x264.jpeg 350w, https://www.akateeminen.com/wp-content/uploads/2021/10/Aleksanterinkatu_1979-500x376.jpeg 500w, https://www.akateeminen.com/wp-content/uploads/2021/10/Aleksanterinkatu_1979-768x578.jpeg 768w, https://www.akateeminen.com/wp-content/uploads/2021/10/Aleksanterinkatu_1979-1024x769.jpeg 1024w, https://www.akateeminen.com/wp-content/uploads/2021/10/Aleksanterinkatu_1979.jpeg 1413w" sizes="(min-width: 0px) and (max-width: 575px) calc(100vw / (12 / 12)), (min-width: 576px) and (max-width: 767px) 540px, (min-width: 768px) and (max-width: 991px) 360px, (min-width: 992px) and (max-width: 1199px) 480px, (min-width: 1200px) and (max-width: 1399px) 570px, (min-width: 1400px) 720px" alt="" style="object-position: 50% 50%;">
-                            </div>
+                    <div class="flex flex-wrap items-center mb-8">
+                        <div class="w-full md:w-1/2 lg:w-1/4 pr-0 md:pr-8 order-2 md:order-1">
+                            <p class="mb-0">Första butikens ledord ”Litteraturen är livets krydda” blev även hela Akademens affärsidé – att erbjuda kvalitetsböcker, högklassig service och inspiration för sin kundkrets.</p>
+                        </div>
+                        <div class="w-full md:w-1/2 lg:w-3/4 pl-0 md:pl-8 order-1 md:order-2">
+                            <img src="https://www.akateeminen.com/wp-content/uploads/2021/10/Aleksanterinkatu_1979.jpeg" alt="" class="w-full">
                         </div>
                     </div>
                 </div>
@@ -97,6 +90,7 @@
         </article>
     </div>
 </section>
+
 
 
 
