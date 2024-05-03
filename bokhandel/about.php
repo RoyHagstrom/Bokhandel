1ddd38052c46a8079d5b971ae6c7f88bb0b5f2a5
Roy.h@mail.com <Roy.h@mail.com>
2024-05-03 10:06:57 +0300

    10:06

---
+++ bokhandel/about.php
@@ -0,0 +1,8 @@
+<div class="bg-white text-black px-4 py-20 flex flex-col justify-center items-center sm:px-6 lg:px-8">
+    <h1 class="text-2xl font-bold mb-4 sm:text-3xl">About Us</h1>
+
+  <h1>About Us</h1>
+  <p>Bokhandel is a small bookstore located in Ekenäs, Finland. We love books and we love our customers. Our store is home to a diverse collection of books, ranging from fiction to non-fiction and literature from different genres. Here at Bokhandel, we believe that reading is a vital part of life and that books have the power to transform us.</p>
+  <p>Our store is located in Pehr Sommars Gata 14, Ekenäs, Finland.</p>
+
+</div>
