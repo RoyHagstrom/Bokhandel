ad1e920be9f30ec984b636f1499722ce403f3714
Roy.h@mail.com <Roy.h@mail.com>
2024-05-03 10:44:48 +0300

    10:44

--- bokhandel/about.php
+++ bokhandel/about.php
@@ -41,6 +41,244 @@ include 'Includes/header.php';
             </div>
         </div>
 
+
+
+        <section id="content" role="main" class="content-wrapper">
+				<div class="content-wrapper">
+		<article class="post-type-page" id="post-288">
+			<section class="article-content">
+				<div class="article-body page-body pt-4">
+											<h1 class="aka-display aka-title title mb-4">Historia</h1>
+										
+<p class="has-primary-1-color has-text-color">Akademiska Bokhandeln blev en del av Bonnier Books i oktober 2015 och har sedan dess fortsatt att fungera självständigt som en inspirerande mötesplats för alla bokälskare. Vi är engagerade i läsning och livslångt lärande. Även vår långa historia säger det – böcker har alltid haft en speciell plats i våra hjärtan. <br><br>Akademens webbplats har uppdaterats flera gånger under åren, senast i oktober 2021. Akademens logo och visuella utseende förnyades i samband med företagets brandförnyelse. Vår förhoppning med&nbsp;förnyelsen&nbsp;är att&nbsp;Akademen ska&nbsp;kunna betjäna sina kunder och samarbetspartners ännu bättre.</p>
+
+
+
+<h2 class="wp-block-heading">1890-1930-talet&nbsp;</h2>
+
+
+
+<p>Akademiska Bokhandeln grundades sommaren 1893, och hade från första början som målsättning att kunna erbjuda sina kunder ett brett urval litteratur på både finska och svenska, och dessutom även utländska titlar, vilket var en sällsynthet i Helsingfors på den tiden. För Akademens grundare Alvar Renqvist och Gösta Branders var bokhandeln inte bara ett företag, utan även ett bidrag för folkbildningen.</p>
+
+
+
+                    
+                
+
+
+    
+<div class="aka-block">
+    <div class="aka-double-column gx-0 aka-image-by-text type-1-3 align-left row depth-0 align-items-center">
+         
+        <div class="col-12 col-md-6 col-lg-9 column-left">
+            
+                <img srcset="https://www.akateeminen.com/wp-content/uploads/2021/10/Akateeminen_aleksanterinkadulla1920-150x150.jpeg 150w, https://www.akateeminen.com/wp-content/uploads/2021/10/Akateeminen_aleksanterinkadulla1920-350x257.jpeg 350w, https://www.akateeminen.com/wp-content/uploads/2021/10/Akateeminen_aleksanterinkadulla1920-500x368.jpeg 500w, https://www.akateeminen.com/wp-content/uploads/2021/10/Akateeminen_aleksanterinkadulla1920-768x565.jpeg 768w, https://www.akateeminen.com/wp-content/uploads/2021/10/Akateeminen_aleksanterinkadulla1920-1024x753.jpeg 1024w, https://www.akateeminen.com/wp-content/uploads/2021/10/Akateeminen_aleksanterinkadulla1920.jpeg 1444w" sizes="(min-width: 0px) and (max-width: 575px) calc(100vw / (12 / 12)), (min-width: 576px) and (max-width: 767px) 540px, (min-width: 768px) and (max-width: 991px) 360px, (min-width: 992px) and (max-width: 1199px) 480px, (min-width: 1200px) and (max-width: 1399px) 570px, (min-width: 1400px) 720px" alt="" style="object-position: 50% 50%;">
+            </div>
+                <div class="col-12 col-md-6 col-lg-3 column-right">
+                
+
+<p>Gösta Branders hade rest runt i Europa och under sina affärsresor bekantat sig med bokaffärer nere på kontinenten, och tog med sig av dessa&nbsp;influenser till Akademen.&nbsp;Utrymmen skulle vara rymliga,&nbsp;urvalet brett&nbsp;och&nbsp;böckerna framme för&nbsp;kunderna&nbsp;att bläddra i. Den kosmopolitiska stämningen och det breda utbudet av europeisk litteratur&nbsp;gjorde&nbsp;Akademen populär bland universitetsfolket och stadens intellektuella.&nbsp;&nbsp;&nbsp;</p>
+
+
+        </div>
+            </div>
+</div>
+
+
+                    
+                
+
+
+    
+<div class="aka-block">
+    <div class="aka-double-column gx-0 aka-image-by-text type-1-3 align-right row depth-0 align-items-center">
+                <div class="col-12 col-md-6 col-lg-3 column-right">
+                
+
+<p>Akademens första butik öppnades på Alexandersgatan 9, men Branders tyckte snart den blev för liten för hans vision. Under 1900-talets första årtionden flyttade bokhandeln runt på ett par olika adresser på Alexandersgatan, innan Akademen 1930 blev uppköpt av OY Stockmann AB och då flyttade in i Stockmanns varuhus på Alexandersgatan 52.</p>
+
+
+        </div>
+         
+        <div class="col-12 col-md-6 col-lg-9 column-left">
+                <img srcset="https://www.akateeminen.com/wp-content/uploads/2021/10/jouluaatto_aleksanterinkadulla1924-150x150.jpeg 150w, https://www.akateeminen.com/wp-content/uploads/2021/10/jouluaatto_aleksanterinkadulla1924-350x258.jpeg 350w, https://www.akateeminen.com/wp-content/uploads/2021/10/jouluaatto_aleksanterinkadulla1924-500x369.jpeg 500w, https://www.akateeminen.com/wp-content/uploads/2021/10/jouluaatto_aleksanterinkadulla1924-768x567.jpeg 768w, https://www.akateeminen.com/wp-content/uploads/2021/10/jouluaatto_aleksanterinkadulla1924-1024x756.jpeg 1024w, https://www.akateeminen.com/wp-content/uploads/2021/10/jouluaatto_aleksanterinkadulla1924-scaled.jpeg 2048w" sizes="(min-width: 0px) and (max-width: 575px) calc(100vw / (12 / 12)), (min-width: 576px) and (max-width: 767px) 540px, (min-width: 768px) and (max-width: 991px) 360px, (min-width: 992px) and (max-width: 1199px) 480px, (min-width: 1200px) and (max-width: 1399px) 570px, (min-width: 1400px) 720px" alt="" style="object-position: 50% 50%;">
+    
+        </div>
+            </div>
+</div>
+
+
+<p>Bland kulturvänner i Helsingfors väckte sammanslagningen med Stockmann i början både tvivel och missnöje, många ifrågasatte hur varuhuset skulle kunna gynna bokhandelns verksamhet. Genom att tydligt avgränsa bokhandelns utrymmen från resten av varuhuset kom man fram till en kompromiss som många var nöjda med – och dessutom hittade nya kunder till Akademen, inte minst eftersom tidskriftssortimentet kunde utökas rejält.</p>
+
+
+
+<h2 class="is-style-default wp-block-heading">1940-talet: Akademen under kriget&nbsp;</h2>
+
+
+
+<p>Under andra världskriget hörde böcker till de varor som inte ransonerades, vilket gjorde att böcker sålde bra, och Akademens försäljning under åren 1940 – 1944 räknas ha gått upp med 190 %. Inhemska författare så som&nbsp;Mika Waltari, Erkki&nbsp;Palolampi, Toivo&nbsp;Pekkanen,&nbsp;Iiris&nbsp;Uurto&nbsp;och Martti&nbsp;Merenmaa&nbsp;var populära, och lästes flitigt såväl på hemmaplan som ute vid fronten.&nbsp;&nbsp;</p>
+
+
+
+                    
+                
+
+
+    
+<div class="aka-block">
+    <div class="aka-double-column gx-0 aka-image-by-text type-1-3 align-right row depth-0 align-items-center">
+                <div class="col-12 col-md-6 col-lg-3 column-right">
+                
+
+<blockquote class="wp-block-quote is-layout-flow wp-block-quote-is-layout-flow"><p>Akademens skyltfönster splittrades flera gånger under fortsättningskriget, och de mindre fönsterrutorna som lades in 1944 ersattes med stora skyltfönster först 1950.</p></blockquote>
+
+
+        </div>
+         
+        <div class="col-12 col-md-6 col-lg-9 column-left">
+                <img srcset="https://www.akateeminen.com/wp-content/uploads/2021/08/csm_aleksanterinkatu7_1920_2aka_22_56c8c7d504_3-150x150.jpg 150w, https://www.akateeminen.com/wp-content/uploads/2021/08/csm_aleksanterinkatu7_1920_2aka_22_56c8c7d504_3-350x219.jpg 350w, https://www.akateeminen.com/wp-content/uploads/2021/08/csm_aleksanterinkatu7_1920_2aka_22_56c8c7d504_3-500x313.jpg 500w, https://www.akateeminen.com/wp-content/uploads/2021/08/csm_aleksanterinkatu7_1920_2aka_22_56c8c7d504_3-768x480.jpg 768w, https://www.akateeminen.com/wp-content/uploads/2021/08/csm_aleksanterinkatu7_1920_2aka_22_56c8c7d504_3-1024x640.jpg 1024w, https://www.akateeminen.com/wp-content/uploads/2021/08/csm_aleksanterinkatu7_1920_2aka_22_56c8c7d504_3.jpg 1340w" sizes="(min-width: 0px) and (max-width: 575px) calc(100vw / (12 / 12)), (min-width: 576px) and (max-width: 767px) 540px, (min-width: 768px) and (max-width: 991px) 360px, (min-width: 992px) and (max-width: 1199px) 480px, (min-width: 1200px) and (max-width: 1399px) 570px, (min-width: 1400px) 720px" alt="" style="object-position: 50% 50%;">
+    
+        </div>
+            </div>
+</div>
+
+
+<h2 class="wp-block-heading">1960-talet&nbsp;&nbsp;Akademen flyttade till Bokhuset</h2>
+
+
+
+<p>Under 1960-talet internationaliserades Finland med fart, och detta syntes också i Akademens urval, vars internationella avdelning, särskilt den engelskspråkiga utökades.&nbsp;&nbsp;</p>
+
+
+
+                    
+                
+
+
+    
+<div class="aka-block">
+    <div class="aka-double-column gx-0 aka-image-by-text type-1-3 align-left row depth-0 align-items-center">
+         
+        <div class="col-12 col-md-6 col-lg-9 column-left">
+            
+                <img srcset="https://www.akateeminen.com/wp-content/uploads/2021/10/stockmann_rakentaa3-150x150.jpeg 150w, https://www.akateeminen.com/wp-content/uploads/2021/10/stockmann_rakentaa3-350x263.jpeg 350w, https://www.akateeminen.com/wp-content/uploads/2021/10/stockmann_rakentaa3-500x376.jpeg 500w, https://www.akateeminen.com/wp-content/uploads/2021/10/stockmann_rakentaa3-768x578.jpeg 768w, https://www.akateeminen.com/wp-content/uploads/2021/10/stockmann_rakentaa3-1024x770.jpeg 1024w, https://www.akateeminen.com/wp-content/uploads/2021/10/stockmann_rakentaa3-scaled.jpeg 2048w" sizes="(min-width: 0px) and (max-width: 575px) calc(100vw / (12 / 12)), (min-width: 576px) and (max-width: 767px) 540px, (min-width: 768px) and (max-width: 991px) 360px, (min-width: 992px) and (max-width: 1199px) 480px, (min-width: 1200px) and (max-width: 1399px) 570px, (min-width: 1400px) 720px" alt="" style="object-position: 50% 50%;">
+            </div>
+                <div class="col-12 col-md-6 col-lg-3 column-right">
+                
+
+<p>1966 inleddes sprängningsarbetet för det nya Bokhuset, ritat av Alvar Aalto för Akademiska Bokhandeln, på Kinopalatsets gamla tomt. Hösten 1969 flyttade Akademen in på sin nuvarande adress mittemot Stockmann och Svenska teatern. Akademens personal ville själv ansvara för flytten, som sköttes nästan helt och hållet under en helg. Upp till 6000 lådor med böcker förflyttades från den gamla butiken till de nya lokalerna. Bokhandeln var stängd endast en dag, lördagen den 13.10.1969, och redan därpå följande måndag firades öppningen.</p>
+
+
+        </div>
+            </div>
+</div>
+
+
+                    
+                
+
+
+    
+<div class="aka-block">
+    <div class="aka-double-column gx-0 aka-image-by-text type-1-3 align-right row depth-0 align-items-center">
+                <div class="col-12 col-md-6 col-lg-3 column-right">
+                
+
+<p>Bokhusets öppningsfest var en riktig kulturhändelse: flaggorna på Esplanaden var hissade och eldkorgarna vid dörren sprakade när undervisningsministern Johannes Virolainen öppnade butiken, de inbjudna gästerna var uppemot tusen. Hedersgäst var Akademens mångåriga stamkund och presidentens hustru, författaren Sylvi Kekkonen. Under öppningsfesten fick också personalen njuta med varsitt glas champagne.</p>
+
+
+        </div>
+         
+        <div class="col-12 col-md-6 col-lg-9 column-left">
+                <img srcset="https://www.akateeminen.com/wp-content/uploads/2021/10/kirjatalon_avajaiset1969-150x150.jpeg 150w, https://www.akateeminen.com/wp-content/uploads/2021/10/kirjatalon_avajaiset1969-350x280.jpeg 350w, https://www.akateeminen.com/wp-content/uploads/2021/10/kirjatalon_avajaiset1969-500x401.jpeg 500w, https://www.akateeminen.com/wp-content/uploads/2021/10/kirjatalon_avajaiset1969-768x615.jpeg 768w, https://www.akateeminen.com/wp-content/uploads/2021/10/kirjatalon_avajaiset1969-1024x820.jpeg 1024w, https://www.akateeminen.com/wp-content/uploads/2021/10/kirjatalon_avajaiset1969-scaled.jpeg 2048w" sizes="(min-width: 0px) and (max-width: 575px) calc(100vw / (12 / 12)), (min-width: 576px) and (max-width: 767px) 540px, (min-width: 768px) and (max-width: 991px) 360px, (min-width: 992px) and (max-width: 1199px) 480px, (min-width: 1200px) and (max-width: 1399px) 570px, (min-width: 1400px) 720px" alt="" style="object-position: 50% 50%;">
+    
+        </div>
+            </div>
+</div>
+
+
+                    
+                
+
+
+    
+<div class="aka-block">
+    <div class="aka-double-column gx-0 aka-image-by-text type-1-3 align-left row depth-0 align-items-center">
+         
+        <div class="col-12 col-md-6 col-lg-9 column-left">
+            
+                <img srcset="https://www.akateeminen.com/wp-content/uploads/2021/08/csm_kirjatalo_old_jouluaka_1074-1-150x150.jpg 150w, https://www.akateeminen.com/wp-content/uploads/2021/08/csm_kirjatalo_old_jouluaka_1074-1-350x281.jpg 350w, https://www.akateeminen.com/wp-content/uploads/2021/08/csm_kirjatalo_old_jouluaka_1074-1-500x401.jpg 500w, https://www.akateeminen.com/wp-content/uploads/2021/08/csm_kirjatalo_old_jouluaka_1074-1-768x616.jpg 768w, https://www.akateeminen.com/wp-content/uploads/2021/08/csm_kirjatalo_old_jouluaka_1074-1-1024x821.jpg 1024w, https://www.akateeminen.com/wp-content/uploads/2021/08/csm_kirjatalo_old_jouluaka_1074-1.jpg 1340w" sizes="(min-width: 0px) and (max-width: 575px) calc(100vw / (12 / 12)), (min-width: 576px) and (max-width: 767px) 540px, (min-width: 768px) and (max-width: 991px) 360px, (min-width: 992px) and (max-width: 1199px) 480px, (min-width: 1200px) and (max-width: 1399px) 570px, (min-width: 1400px) 720px" alt="" style="object-position: 50% 50%;">
+            </div>
+                <div class="col-12 col-md-6 col-lg-3 column-right">
+                
+
+<p>Bokhuset blev snabbt en sevärdhet även på grund av sin arkitektur. Bokhusets främsta material är marmor och koppar. Takfönstrens prisman, som förövrigt kan tolkas som uppslagna böcker, släpper in naturligt ljus i lokalen. Öppningen i mitten av bokhuset är inspirerad av traditionella italienska piazzan, och bildar således också en naturlig träffpunkt.</p>
+
+
+        </div>
+            </div>
+</div>
+
+
+                                             
+                
+
+
+    
+<div class="aka-block">
+    <div class="aka-double-column gx-0 aka-image-by-text type-1-1 align-right row depth-0 align-items-center">
+                <div class="col-12 col-lg-6 column-right">
+                
+
+<h2 class="is-style-default wp-block-heading">1970-talet – Akademens Träffpunkt inleddes</h2>
+
+
+
+<p>Akademens&nbsp;numera&nbsp;klassiska författarintervjuer, inleddes hösten 1970.&nbsp;Träffpunktens allra första gäst var Arvo “Poika” Tuominen, som då var aktuell med “Myrskyn&nbsp;aikaa”, fjärde delen i sina memoarer.&nbsp;&nbsp;</p>
+
+
+        </div>
+         
+        <div class="col-12 col-lg-6 column-left">
+                <img srcset="https://www.akateeminen.com/wp-content/uploads/2021/10/ensimmäinen_kohtaamispaikka-150x150.jpeg 150w, https://www.akateeminen.com/wp-content/uploads/2021/10/ensimmäinen_kohtaamispaikka-350x269.jpeg 350w, https://www.akateeminen.com/wp-content/uploads/2021/10/ensimmäinen_kohtaamispaikka-500x384.jpeg 500w, https://www.akateeminen.com/wp-content/uploads/2021/10/ensimmäinen_kohtaamispaikka-768x589.jpeg 768w, https://www.akateeminen.com/wp-content/uploads/2021/10/ensimmäinen_kohtaamispaikka-1024x786.jpeg 1024w, https://www.akateeminen.com/wp-content/uploads/2021/10/ensimmäinen_kohtaamispaikka-scaled.jpeg 2048w" sizes="(min-width: 0px) and (max-width: 575px) calc(100vw / (12 / 12)), (min-width: 576px) and (max-width: 767px) 540px, (min-width: 768px) and (max-width: 991px) 360px, (min-width: 992px) and (max-width: 1199px) 480px, (min-width: 1200px) and (max-width: 1399px) 570px, (min-width: 1400px) 720px" alt="" style="object-position: 50% 50%;">
+    
+        </div>
+            </div>
+</div>
+
+
+<h2 class="wp-block-heading">1980-talet&nbsp;&nbsp;</h2>
+
+
+
+<p>Under 1980-talet öppnade Akademen butiker i Åbo, Tammerfors och Jyväskylä. År 1989 öppnades även Cafe Alto i&nbsp;Bokhuset i utrymmet där&nbsp;Akademens telefonväxel&nbsp;tidigare huserade.&nbsp;Alvar Aaltos designade möbler hade Stockmann räddat året innan då Café Columbia stängde sina dörrar i Järnhuset.&nbsp;Läs mera om Cafe Altos historia <a href="https://www.akateeminen.com/sv/cafe-aalto/" data-type="page" data-id="229">här</a>.&nbsp;&nbsp; <br><br>1989&nbsp;ordnades Bokens natt, ett evenemang som därefter blivit en klassiker på Akademen. Konceptet blev en omedelbar succé och har ordnats i samband med Konstens natt sedan dess.&nbsp;&nbsp;</p>
+
+
+
+<h2 class="wp-block-heading">1990-talet&nbsp;– Akademens nätförsäljning inleddes</h2>
+
+
+
+<p>Under 1990-talet inleddes&nbsp;nätförsäljning. År 1994&nbsp;startade Akademen en&nbsp;beställningstjänst för företagskunder på internet. Akademen öppnade sin första egna hemsida år 1997 och två år senare öppnades även en webbshop för privatkunder.&nbsp;&nbsp;</p>
+
+
+
+<h2 class="wp-block-heading">2000-talet</h2>
+
+
+
+<p>År 2015 såldes Akademiska Bokhandeln till mediebolaget Bonnier. Akademen startade ett samarbete med Gros Bokhandel i Vasa år 2020.</p>
+
+
+
+<p>Under 2000-talet har Akademen haft flera anledningar till att ordna exceptionella fester. Finlands äldsta bokhandel, Akademiska Bokhandeln, firade sin 125-årsdag den 18.7.2018.&nbsp;Träffpunkten som fyllde 50 år 2020 vittnar om en lång tradition av att utgöra en röst i samhället.&nbsp;Akademen är fortfarande den mötesplats och inspirationskälla som den varit för kulturvänner i snart 130 år.&nbsp;</p>
+
+				</div>
+			</section>
+		</article>
+	</div><!-- /content-wrapper -->
+		</section>
+
     </div>  
 </div>
 
