6c07ff40c8f6f80880c738d761485c03da908f00
Roy.h@mail.com <Roy.h@mail.com>
2024-05-03 10:32:23 +0300

    10:32

--- bokhandel/about.php
+++ bokhandel/about.php
@@ -2,25 +2,22 @@
 include 'Includes/header.php'; 
 ?>
 
-<div class="bg-white text-black px-4 py-20 flex flex-col justify-center items-center sm:px-6 lg:px-8">
+<div class="bg-white text-black px-4 py-20 sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-6xl rounded-lg shadow-md p-8">
 
+        <h1 class="text-3xl sm:text-4xl font-bold text-center mb-8">About Us</h1>
 
-<div class="mx-auto max-w-800 rounded-lg shadow-md p-8 m-8 justify-center items-center ">
-
-    <h1 class="text-2xl font-bold mb-4 sm:text-3xl text-center">About Us</h1>
-
-        <div class="md:flex items-center">
+        <div class="flex flex-col md:flex-row items-center">
             <img src="images/img-02.jpg" alt="Image" class="w-full md:w-1/2 h-auto md:h-80 object-cover rounded-lg mb-6 md:mb-0">
             <p class="leading-relaxed md:ml-6">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus, dolor eget suscipit facilisis, magna felis tempus ligula, at eleifend mi nibh eu nunc. Nullam auctor, velit in fermentum bibendum, metus odio malesuada nulla, eu suscipit nisl nisi in tortor. Ut vel purus est. Sed tincidunt tincidunt mi, ac tincidunt nunc feugiat ac. Vivamus mattis nulla eu nisl rhoncus, vitae cursus justo lacinia. Sed feugiat, elit non vulputate aliquet, ante purus cursus nisl, vel lobortis risus nunc eu mi.
             </p>
         </div>
 
-        
         <div class="mt-8 border-t border-gray-200"></div>
 
         <div class="mt-8 md:flex items-center">
-            <h2 class="text-lg font-bold mb-4 md:mb-0 md:text-left">Lorem ipsum</h2>
+            <h2 class="text-lg font-bold mb-4 md:mb-0 md:text-left">Our Mission</h2>
             <p class="text-lg md:ml-6">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus, dolor eget suscipit facilisis, magna felis tempus ligula, at eleifend mi nibh eu nunc.
             </p>
@@ -40,13 +37,8 @@ include 'Includes/header.php';
             </p>
         </div>
 
-
     </div>  
-
-
 </div>
-
-
 
 <?php
 include 'Includes/footer.php'; 
