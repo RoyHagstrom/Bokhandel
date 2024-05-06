456b8a057908de13501a5bfa6fd786d6378ba1c0
Roy.h@mail.com <Roy.h@mail.com>
2024-05-06 15:04:12 +0300

    15:04

--- bokhandel/singlebook.php
+++ bokhandel/singlebook.php
@@ -228,14 +228,14 @@ if(isset($_SESSION['uname'])){
                 <span class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400"><?php echo $rating . ' out of ' . $outOf; ?></span>
                 
                 <?php if ($voted): ?>
-                    
+
                 <?php else: ?>
 
                     <form method="post" class="inline-flex items-center gap-2">
                     <input type="hidden" name="book" value="<?php echo htmlspecialchars($book['BookID']); ?>">
                     <input type="hidden" name="rating" id="rating" value="<?php echo htmlspecialchars($rating); ?>">
                     <button type="submit" name="submit-rating" value="0.05" class="text-green-600 hover:text-green-800 flex items-center">
-                        <img class="w-8 h-8" src="<?php echo $voted ? "https://www.svgrepo.com/show/506603/thumbs-up-fill.svg" : "https://www.svgrepo.com/show/506603/thumbs-up.svg"; ?>"
+                        <img class="w-8 h-8" src="https://www.svgrepo.com/show/506603/thumbs-up.svg"
                             alt="Thumbs Up">
                     </button>
                 </form>
@@ -244,7 +244,7 @@ if(isset($_SESSION['uname'])){
                     <input type="hidden" name="book" value="<?php echo htmlspecialchars($book['BookID']); ?>">
                     <input type="hidden" name="rating" id="rating" value="<?php echo htmlspecialchars($rating); ?>">
                     <button type="submit" name="submit-rating" value="-0.05" class="text-red-600 hover:text-red-800 flex items-center">
-                        <img class="w-8 h-8" src="<?php echo $voted ? "https://www.svgrepo.com/show/506605/thumbs-down-fill.svg" : "https://www.svgrepo.com/show/506602/thumbs-down.svg"; ?>"
+                        <img class="w-8 h-8" src="https://www.svgrepo.com/show/506602/thumbs-down.svg"
                             alt="Thumbs Down">
                     </button>
                 </form>
