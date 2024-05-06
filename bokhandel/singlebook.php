b5ced74b47269595d257acfc5cc69d670d51ead7
Roy.h@mail.com <Roy.h@mail.com>
2024-05-06 14:40:34 +0300

    14:40

--- bokhandel/singlebook.php
+++ bokhandel/singlebook.php
@@ -252,9 +252,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                 let newRating = (value).toFixed(1); 
                                 console.log('Calculated new rating:', newRating);
                                 
-                                newRating = Math.min(5, Math.max(0, newRating));
-                                console.log('Clamped new rating:', newRating);
-                                
                                 ratingInput.value = newRating; 
                                 console.log('Updated rating input value');
                                 
