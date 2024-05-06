26027253a44cf78f8bcd0be114c353ed266974d0
Roy.h@mail.com <Roy.h@mail.com>
2024-05-06 10:36:26 +0300

    10:36

--- bokhandel/Includes/header.php
+++ bokhandel/Includes/header.php
@@ -214,7 +214,7 @@ include 'db_connection.php';
             <li><a href="books.php" class="text-white dark:text-gray-200 hover:text-gray-300">Books</a></li>
             <li><a href="about.php" class="text-white dark:text-gray-200 hover:text-gray-300">About us</a></li>
             <li><a href="contact.php" class="text-white dark:text-gray-200 hover:text-gray-300">Contact</a></li>
-            <?php if(isset($_SESSION["uname"])){echo '<li><a href="account.php?uid=' . $_SESSION["uid"] . '" class="text-white dark:text-gray-200 hover:text-gray-300">' . $_SESSION["uname"] . '</a></li>';}?>
+            <?php if(isset($_SESSION["uname"])){echo '<li><a href="account.php?uid=' . $_SESSION["uname"] . '" class="text-white dark:text-gray-200 hover:text-gray-300">' . $_SESSION["uname"] . '</a></li>';}?>
                     <?php if(isset($_SESSION["uname"])){echo '<li><a href="logout.php" class="bg-black-900 text-white dark:text-gray-200 hover:bg-black-800 dark:hover:bg-black-400 px-4 py-2 rounded-md">Logout</a></li>';}
                     else
                     { echo ' <li><a href="login.php" class="bg-black-900 text-white dark:text-gray-200 hover:bg-black-800 dark:hover:bg-black-400 px-4 py-2 rounded-md">Login</a></li>';}?>
