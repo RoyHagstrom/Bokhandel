abbcbe577e331aabf66861e34cfaa8e5b0f5f2a9
Roy.h@mail.com <Roy.h@mail.com>
2024-05-06 10:11:32 +0300

    10:11

--- bokhandel/Includes/db_connection.php
+++ bokhandel/Includes/db_connection.php
@@ -1,44 +1,53 @@
 <?php
 
-    define('DB_HOSTS', [
-        'primary' => '172.22.0.4',
-        'local' => 'localhost',
-        'network' => 'novatest.ddns.net'
-    ]);
-    define('DB_PORT', '3306');
-    define('DB_USERNAME', 'test');
-    define('DB_PASSWORD', 'test');
-    define('DB_DATABASE', 'bokhandel');
-    define('DB_CONNECTION_TIMEOUT', 3);
+define('DB_HOSTS', [
+    'local' => '172.22.0.4',
+    'network' => 'novatest.ddns.net'
+]);
+define('DB_PORT', '3306');
+define('DB_USERNAME', 'test');
+define('DB_PASSWORD', 'test');
+define('DB_DATABASE', 'bokhandel');
+define('DB_CONNECTION_TIMEOUT', 3);
 
-    function getDatabaseConnection() {
-        foreach (DB_HOSTS as $host) {
+function getDatabaseConnection() {
+    foreach (DB_HOSTS as $host) {
+        try {
             $conn = new mysqli($host, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
             if (!$conn->connect_error) {
                 $conn->options(MYSQLI_OPT_CONNECT_TIMEOUT, DB_CONNECTION_TIMEOUT);
                 return $conn;
-            } else {
-                $conn->close();
-                throw new Exception("Could not get database connection");
+            }
+        } catch (Exception $e) {
+
+            error_log($e->getMessage());
+        } finally {
+            if (isset($conn) && $conn instanceof mysqli) {
+                $conn->close(); 
             }
         }
-        return null;
     }
+    throw new Exception("Could not get database connection"); 
+}
 
-    
 ini_set('opcache.enable_cli', 1);
 ini_set('opcache.validate_timestamps', 1);
 ini_set('opcache.save_comments', 0);
 ini_set('opcache.enable_file_override', 0);
 ini_set('opcache.fast_shutdown', 1);
 
-    if (session_status() !== PHP_SESSION_ACTIVE) {
-        session_start();
-    }
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
 
+try {
     $conn = getDatabaseConnection();
     if (!$conn) {
         throw new Exception("Could not get database connection");
     }
     $user = new USER($conn);
+} catch (Exception $e) {
+    error_log($e->getMessage());
+}
 
+?>
