cc6b3925cda234fc20fcbfcb1b6ecc694fb3bd57
Roy.h@mail.com <Roy.h@mail.com>
2024-05-06 17:04:00 +0300

    17:03

--- bokhandel/index.php
+++ bokhandel/index.php
@@ -196,9 +196,7 @@ $highest_rated_result = $conn->query($highest_rated_sql);
 <div class="flex items-center mb-6 gap-2">
 
     <h1 class="text-2xl font-semibold">Highest Rated Books</h1>
-    <p class="mt-2 text-sm text-gray-700 dark:text-gray-300 lg:text-base">
-        Our highest rated books are selected based on their reviews from our users. Here are some of our top picks.
-    </p>
+
 
 
     <svg class="w-4 h-4 text-yellow-300 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
@@ -217,6 +215,9 @@ $highest_rated_result = $conn->query($highest_rated_sql);
         <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
     </svg>
 </div>
+<p class="mt-2 text-sm text-gray-700 dark:text-gray-300 lg:text-base">
+        Our highest rated books are selected based on their reviews from our users. Here are some of our top picks.
+    </p>
 
     <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 md:gap-8">
         <?php
