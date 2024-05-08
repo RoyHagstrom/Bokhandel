5f58ca1f98d45c614b457720720d2188609fb604
Roy.h@mail.com <Roy.h@mail.com>
2024-05-08 17:55:12 +0300

    17:55

--- bokhandel/index.php
+++ bokhandel/index.php
@@ -163,7 +163,7 @@ $highest_rated_result = $conn->query($highest_rated_sql);
 </div>
 
 <?php
-$stmt = $conn->query("SELECT * FROM User ORDER BY Rating DESC LIMIT 3");
+$stmt = $conn->query("SELECT * FROM User ORDER BY Rating DESC LIMIT 5");
 while ($user = $stmt->fetch_assoc()) {?>
 <div class="container bg-white p-8 rounded-lg shadow-md w-full sm:w-130 mt-8">
     <h1 class="text-2xl font-semibold mb-6">Users</h1>
