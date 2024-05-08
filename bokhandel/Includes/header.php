a3dd80d638ede58aa6c2402b17d00e86820f9b4a
Roy.h@mail.com <Roy.h@mail.com>
2024-05-08 10:11:52 +0300

    10:11

--- bokhandel/Includes/header.php
+++ bokhandel/Includes/header.php
@@ -58,8 +58,8 @@ include 'db_connection.php';
 
 
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="../Images/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="../Images/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png">
 
 
     <link rel="dns-prefetch" href="https://cdn.jsdelivr.net">
