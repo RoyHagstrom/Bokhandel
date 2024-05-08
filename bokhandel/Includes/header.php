781b65dfcfd9078deaa4e251ad5694bcffe44b0d
Roy.h@mail.com <Roy.h@mail.com>
2024-05-08 10:12:38 +0300

    10:12

--- bokhandel/Includes/header.php
+++ bokhandel/Includes/header.php
@@ -57,7 +57,6 @@ include 'db_connection.php';
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png">
 
