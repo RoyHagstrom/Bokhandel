a51b4ad1fb49dadd773959a1fc2f492270507975
Roy.h@mail.com <Roy.h@mail.com>
2024-05-13 14:56:05 +0300

    14:56

--- bokhandel/singlebook.php
+++ bokhandel/singlebook.php
@@ -211,7 +211,8 @@ $other_books_result = $conn->query($other_books_sql);
             <div class="flex">
                 <?php
 
-                $rating = $book['Rating'];
+                $rating = number_format($book['Rating'], 1);
+
                 
                 $outOf = 5;
                 $filledStars = floor($rating);
