491849d4995ced764e84776533b0568dd00db6e8
Roy.h@mail.com <Roy.h@mail.com>
2024-05-13 15:39:50 +0300

    15:39

--- bokhandel/login.php
+++ bokhandel/login.php
@@ -42,6 +42,7 @@ ob_end_flush();
                         <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                         <input type="password" name="Password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                     </div>
+                    <p>Don't have an account? <a href="register.php" class="text-blue-600 hover:underline">Register</a></p>
                     <p class="bg-gray-950 text-black"><?php if(isset($loginReturn)){echo $loginReturn;} ?></p>
                     <button type="submit" name="article-submit" class="w-full text-white bg-black hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                 </form>
