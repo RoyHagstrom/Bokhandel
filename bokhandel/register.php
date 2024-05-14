12cfc492f7174dc426e4049ab23c1bd4784a7f91
Roy.h@mail.com <Roy.h@mail.com>
2024-05-14 16:49:25 +0300

    16:49

--- bokhandel/register.php
+++ bokhandel/register.php
@@ -69,7 +69,8 @@ else{
                 <?php endif; ?>
 
 
-
+                    <input type="checkbox" id="privacy-policy" name="privacy-policy" required>
+                    <label for="privacy-policy" class="text-sm text-gray-700 dark:text-white">By registering, you confirm that you have read the Privacy Policy</label>
                     <p class="bg-gray-950 text-black"><?php if(isset($registerReturn)){echo $registerReturn;} ?></p>
                     <button type="submit" name="register-submit" class="w-full text-white bg-black hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Register User</button>
                 </form>
