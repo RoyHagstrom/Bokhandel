d5f4c0ed910c9e4315c7cb0a2099a48340055e23
Roy.h@mail.com <Roy.h@mail.com>
2024-05-14 15:27:13 +0300

    15:27

--- bokhandel/register.php
+++ bokhandel/register.php
@@ -31,7 +31,7 @@ else{
 ?>
 
 <section class="bg-gray-50 dark:bg-gray-900 mb-4">
-    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:min-h-screen lg:py-0">
         <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
             Bokhandel    
         </a>
