1e5b8c9b6b9182cec78313169a0288a5e6a86074
Roy.h@mail.com <Roy.h@mail.com>
2024-05-17 11:25:23 +0300

    11:25

--- bokhandel/register.php
+++ bokhandel/register.php
@@ -62,6 +62,7 @@ else{
                         <label for="role" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
                         <select id="role" name="role" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                             <option value="">Select Role</option>
+                            <option value="User">User</option>
                             <option value="Regular">Regular</option>
                             <option value="Admin">Admin</option>
                         </select>
