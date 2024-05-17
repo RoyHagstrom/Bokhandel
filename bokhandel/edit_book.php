5e95e50e1b3f8020a01d8120308606c71d462bf7
Roy.h@mail.com <Roy.h@mail.com>
2024-05-17 16:32:22 +0300

    16:32

--- bokhandel/edit_book.php
+++ bokhandel/edit_book.php
@@ -180,7 +180,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <select id="Series" name="SeriesID" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option value="">Select Series</option>
                         <?php foreach ($series as $s): ?>
-                            <option value="<?php echo $s['SeriesName']; ?>" <?php echo isset($_POST['SeriesID']) && $_POST['SeriesID'] == $s['SeriesName'] ? 'selected' : (isset($bookData['SeriesID']) && $bookData['SeriesID'] == $s['SeriesName'] ? 'selected' : ''; ?>>
+                            <option value="<?php echo $s['SeriesName']; ?>" <?php echo (isset($_POST['series']) && $_POST['series'] == $s['SeriesName']) || (isset($bookData['SeriesID']) && $bookData['SeriesID'] == $s['SeriesName']) ? 'selected' : ''; ?>>
                                 <?php echo htmlspecialchars($s['SeriesName'], ENT_QUOTES); ?>
                             </option>
                         <?php endforeach; ?> 
