e4ee7b548dd537aabe49c8ec4326fa10dcd23e1b
Roy.h@mail.com <Roy.h@mail.com>
2024-05-20 10:08:03 +0300

    10:07

--- bokhandel/Includes/db_connection.php
+++ bokhandel/Includes/db_connection.php
@@ -1,43 +1,47 @@
 <?php
 
-    define('DB_HOSTS', [
-        'local' => 'mysql-db-1',
-        'network' => 'novatest.ddns.net'
-    ]);
-    define('DB_PORT', '3306');
-    define('DB_USERNAME', 'test');
-    define('DB_PASSWORD', 'test');
-    define('DB_DATABASE', 'bokhandel');
-    define('DB_CONNECTION_TIMEOUT', 3);
+define('DB_HOSTS', [
+    'local' => 'mysql-db-1',
+    'network' => 'novatest.ddns.net'
+]);
+define('DB_PORT', '3306');
+define('DB_USERNAME', 'test');
+define('DB_PASSWORD', 'test');
+define('DB_DATABASE', 'bokhandel');
+define('DB_CONNECTION_TIMEOUT', 3);
 
-    function getDatabaseConnection() {
-        foreach (DB_HOSTS as $host) {
-            $conn = new mysqli($host, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
-            if (!$conn->connect_error) {
-                $conn->options(MYSQLI_OPT_CONNECT_TIMEOUT, DB_CONNECTION_TIMEOUT);
-                return $conn;
-            } else {
-                $conn->close();
-                throw new Exception("Could not get database connection");
-            }
+/**
+ * @return mysqli
+ * @throws Exception 
+ */
+function getDatabaseConnection() {
+    foreach (DB_HOSTS as $host) {
+        $conn = new mysqli($host, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+        if ($conn->connect_errno === 0) {
+            $conn->options(MYSQLI_OPT_CONNECT_TIMEOUT, DB_CONNECTION_TIMEOUT);
+            return $conn;
+        } else {
+            error_log("Connection failed to host $host: " . $conn->connect_error);
         }
-        return null;
     }
+    throw new Exception("Could not get database connection");
+}
 
-    
 ini_set('opcache.enable_cli', 1);
 ini_set('opcache.validate_timestamps', 1);
 ini_set('opcache.save_comments', 0);
 ini_set('opcache.enable_file_override', 0);
 ini_set('opcache.fast_shutdown', 1);
 
-    if (session_status() !== PHP_SESSION_ACTIVE) {
-        session_start();
-    }
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
 
+try {
     $conn = getDatabaseConnection();
-    if (!$conn) {
-        throw new Exception("Could not get database connection");
-    }
     $user = new USER($conn);
-
+} catch (Exception $e) {
+    error_log($e->getMessage());
+    die("A critical error occurred. Please try again later.");
+}
+?>
