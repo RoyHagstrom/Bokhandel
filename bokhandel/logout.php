350233a7e4f91e1e1c5e30c0cfdb732550706c98
Roy.h@mail.com <Roy.h@mail.com>
2024-05-20 14:43:46 +0300

    14:43

--- bokhandel/logout.php
+++ bokhandel/logout.php
@@ -5,15 +5,14 @@ include 'Includes/db_connection.php';
 
 session_destroy();
 
+if (isset($_SERVER['HTTP_COOKIE'])) {
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+    foreach($cookies as $cookie) {
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time() - 3600, '/');
+    }
+}
 
-
-
-
-
-
-
-    $user->redirect("index.php");
-
-
-
+$user->redirect("index.php");
 ?>
