0c64f536c79a38f50ce8a44a1135e9942799d1e5
Roy.h@mail.com <Roy.h@mail.com>
2024-05-20 17:50:07 +0300

    17:50

--- bokhandel/index.php
+++ bokhandel/index.php
@@ -171,15 +171,15 @@ $adult_books_result = $conn->query($adult_books_sql);
 $stmt = $conn->query("SELECT * FROM User ORDER BY Rating DESC LIMIT 5");
 $rank = 1;
 ?>
-<div class="container bg-white p-8 rounded-lg shadow-md w-full max-w-4xl mx-auto mt-8">
+<div class="container bg-white p-8 rounded-lg shadow-md w-full max-w-6xl mx-auto mt-8">
     <h1 class="text-2xl sm:text-3xl font-semibold mb-4">Top Rated Authors</h1>
     <p class="leading-relaxed text-sm sm:text-base lg:text-lg mb-6">This is the top 5 authors based on their ratings.</p>
-    <div class="text-gray-700 dark:text-gray-300 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-6 gap-6">
+    <div class="text-gray-700 dark:text-gray-300 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6">
         <?php while ($user = $stmt->fetch_assoc()) { ?>
             <a class="block" href="my_books.php?uid=<?= $user['Username'] ?>">
-                <div class="flex items-center border-b-2 border-gray-200 pb-4 mb-4">
-                    <img src="<?= $user['Image'] ?>" alt="<?= $user['Username'] ?>" class="rounded-full h-16 w-16 sm:h-20 sm:w-20 md:h-24 md:w-24 lg:h-28 lg:w-28 object-cover mr-4 sm:mr-6 md:mr-8 lg:mr-10" />
-                    <div>
+                <div class="flex flex-col items-center border-b-2 border-gray-200 pb-4 mb-4">
+                    <img src="<?= $user['Image'] ?>" alt="<?= $user['Username'] ?>" class="rounded-full h-16 w-16 sm:h-20 sm:w-20 md:h-24 md:w-24 lg:h-28 lg:w-28 object-cover mb-4" />
+                    <div class="text-center">
                         <h2 class="text-lg sm:text-xl md:text-2xl lg:text-3xl font-semibold text-black">#<?= $rank ?> <?= $user['Username'] ?></h2>
                         <p class="text-gray-600"><?= $user['Rating'] ?>/5</p>
                     </div>
@@ -189,6 +189,7 @@ $rank = 1;
         <?php } ?>
     </div>
 </div>
+
 
 
 
